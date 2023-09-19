f221694265156705e7b538afddb8d2bd613e88ea
NDKhanh1996 <ndkhanh1996@gmail.com>
2023-09-19 12:18:50 +0700

    pause play and auto chuyen bai tiep complete

--- src/components/playBar/PlayBar.js
+++ src/components/playBar/PlayBar.js
@@ -3,25 +3,26 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import {useEffect, useRef, useState} from "react";
 import {Audio} from 'expo-av';
 import SongService from "../../services/song.service";
-import {useDispatch,useSelector} from 'react-redux'
-import {setListSong} from "../../redux/feature/songQueueSlice";
+import {useDispatch, useSelector} from 'react-redux'
+import {setCurrentSong, setListSong} from "../../redux/feature/songQueueSlice";
+
+const soundObject = new Audio.Sound();
+let songIndex = 0;
 
 export function PlayBar({id, entity}) {
     const [isPlaying, setIsPlaying] = useState(false);
-    const sound = useRef();
-    const [songArray, setSongArray] = useState([]);
-    const [songData, setSongData] = useState(null);
     const dispatch = useDispatch();
+    const songArray = useSelector(state => state.songQueue.listSong);
+    const currentSong = useSelector(state => state.songQueue.currentSong);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 if (entity === "playlist") {
-                    const data = await SongService.getPlaylist(id);
-                    dispatch(setListSong(data.data["playlist"]["songs"]));
-                    setSongArray(data.data["playlist"]["songs"]);
-                    if (!songData) {
-                        setSongData(data.data["playlist"]["songs"][0]);
+                    const response = await SongService.getPlaylist(id);
+                    dispatch(setListSong(response.data["playlist"]["songs"]));
+                    if (!currentSong) {
+                        dispatch(setCurrentSong(response.data["playlist"]["songs"][0]));
                     }
                 }
             } catch (e) {
@@ -32,53 +33,52 @@ export function PlayBar({id, entity}) {
         fetchData().then();
     }, []);
 
-
-    const playSound = async () => {
-        try {
-            if (!sound.current && songArray.length > 0) {
-                const soundPromises = songArray.map(async (song) => {
-                    const {sound: newSound} = await Audio.Sound.createAsync({
-                        uri: song["fileURL"],
-                    });
-                    return newSound;
-                });
-
-                const newSounds = await Promise.all(soundPromises);
-                sound.current = newSounds[0];
-
-                setIsPlaying(true);
-                await sound.current.playAsync();
-            } else {
-                setIsPlaying(true);
-                await sound.current.playAsync();
+    async function playSound() {
+        setIsPlaying(true);
+        if (songIndex < songArray.length) {
+            try {
+                const status = await soundObject.getStatusAsync();
+                if (!status.isLoaded) {
+                    const song = songArray[songIndex];
+                    await soundObject.loadAsync({ uri: song["fileURL"] });
+                    dispatch(setCurrentSong(song));
+                }
+                await soundObject.playAsync();
+            } catch (error) {
+                console.log(error);
             }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-
-    const pauseSound = async () => {
-        if (sound.current) {
-            await sound.current.pauseAsync();
+        } else {
+            console.log("out of list song")
             setIsPlaying(false);
         }
     }
 
-    useEffect(() => {
-        return sound.current
-            ? () => {
-                console.log('Unloading Sound');
-                sound.current.unloadAsync();
+    soundObject.setOnPlaybackStatusUpdate(async playbackStatus => {
+        try {
+            if (playbackStatus.didJustFinish && !playbackStatus.isLooping) {
+                songIndex++;
+                await soundObject.unloadAsync();
+                await playSound();
             }
-            : undefined;
-    }, [sound]);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    async function pauseSound() {
+        try {
+            await soundObject.pauseAsync();
+            setIsPlaying(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
-        songData && (
+        currentSong && (
             <View className="h-14 mb-0.5 bg-zinc-800/90 flex-row justify-between">
-                <Image className="h-full aspect-square" source={{uri: songData.avatar}}/>
-                <Text className="text-white self-center">{songData["songName"]}</Text>
+                <Image className="h-full aspect-square" source={{uri: currentSong.avatar}}/>
+                <Text className="text-white self-center">{currentSong["songName"]}</Text>
                 <TouchableOpacity className="mr-2 self-center" onPress={!isPlaying ? playSound : pauseSound}>
                     {!isPlaying ? (
                         <Ionicons name="play" size={30} color={"white"}/>
--- src/redux/feature/songQueueSlice.js
+++ src/redux/feature/songQueueSlice.js
@@ -1,7 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit";
 const initialState = {
     listSong: [],
-    currentSong: {}
+    currentSong: null,
 }
 
 const songQueueSlice = createSlice({
